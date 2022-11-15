9c1acd55121d82b8a66a22fd9f7aa702a15b0ca9
sun baolu <baolu_sun@163.com>
2022-11-15 20:33:33 +0800

    script

--- 项目练习文件夹/Vue/vue_cli/src/App.vue
+++ 项目练习文件夹/Vue/vue_cli/src/App.vue
@@ -1,16 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App"> </HelloWorld>
-  <AppTODOLis> </AppTODOLis>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App"> </HelloWorld>
+    <AppTODOLis> </AppTODOLis>
+    <el-row class="mb-4">
+      <el-button @click="handlebutclick">Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+      <el-button plain>Plain</el-button>
+    </el-row>
+  </div>
 </template>
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 import AppTODOLis from "./components/AppTODOLis.vue";
+
 export default {
   name: "App",
   components: {
     HelloWorld,
     AppTODOLis,
+  },
+  methods: {
+    handlebutclick() {
+      alert("handlebutclick");
+    },
   },
 };
 </script>
